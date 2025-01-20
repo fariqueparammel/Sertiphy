a7c7c691052a22f45714b95fcabd83e5643df6b8
farique <fariqueparammel@gmail.com>
2025-01-20 16:32:33 +0530

    adding image to canvas done

--- public/js/filament/konvaScript.js
+++ public/js/filament/konvaScript.js
@@ -14,23 +14,23 @@ var stage = new Konva.Stage({
 // then create layer
 var layer = new Konva.Layer();
 stage.add(layer);
-// create our shape
-var circle = new Konva.Circle({
-    x: stage.width() / 2,
-    y: stage.height() / 2,
-    radius: 70,
-    fill: "red",
-    stroke: "black",
-    strokeWidth: 4,
-});
+// // create our shape
+// var circle = new Konva.Circle({
+//     x: stage.width() / 2,
+//     y: stage.height() / 2,
+//     radius: 70,
+//     fill: "red",
+//     stroke: "black",
+//     strokeWidth: 4,
+// });
 
-// add the shape to the layer
-layer.add(circle);
+// // add the shape to the layer
+// layer.add(circle);
 
 // add the layer to the stage
 
 // draw the image
-layer.draw();
+// layer.draw();
 
 function fitStageIntoParentContainer() {
     var container = document.querySelector("#konvaCanvas");
@@ -51,13 +51,25 @@ window.addEventListener("resize", fitStageIntoParentContainer);
 
 fitStageIntoParentContainer();
 
-function myFunction(fileUrl) {
-    console.log("Button clicked! File URL:", fileUrl);
+function getCurrentImageUrl(imageUrl) {
+    console.log("file url", imageUrl);
+    var imageObj = new Image();
+    imageObj.onload = function () {
+        var templateImage = new Konva.Image({
+            image: imageObj,
+            width: sceneWidth,
+            height: sceneHeight,
+        });
+
+        // add the shape to the layer
+        layer.add(templateImage);
+    };
+    imageObj.src = imageUrl;
 }
 
 document.querySelectorAll(".image-button").forEach((button) => {
     button.addEventListener("click", function () {
-        const fileUrl = this.getAttribute("data-file-url");
-        myFunction(fileUrl);
+        const imageUrl = this.getAttribute("data-file-url");
+        getCurrentImageUrl(imageUrl);
     });
 });
--- resources/views/filament/app/components/preset-template.blade.php
+++ resources/views/filament/app/components/preset-template.blade.php
@@ -1,8 +1,7 @@
 <div class="preset-template-images">
     @foreach ($files as $file)
         <div class="image-container">
-            <x-filament::button class="image-button" data-file-url="{{ asset('storage/' . $file) }} "
-                x-on:click="alert('Hello World!')">
+            <x-filament::button class="image-button" data-file-url="{{ asset('storage/' . $file) }} ">
                 <img src="{{ asset('storage/' . $file) }}" alt="Preset Template" class="template-image" loading="lazy">
             </x-filament::button>
         </div>
